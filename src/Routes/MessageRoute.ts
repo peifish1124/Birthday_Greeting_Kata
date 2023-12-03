ebae88799a6bd4eb93edbeddcded8744ba9ca4fc
peifish1124 <sophiachen1124@gmail.com>
2023-12-03 14:39:38 +0800

    feat: complete verson-2 api

--- src/Routes/MessageRoute.ts
+++ src/Routes/MessageRoute.ts
@@ -4,7 +4,7 @@ import MessageController from "../Controllers/MessageController.js";
 import MockMemberHandler from "../Services/MemberHandlers/MockMemberHandler.js";
 import MysqlMemberHandler from "../Services/MemberHandlers/MysqlMemberHandler.js";
 import MessageHandlerVersion1 from "../Services/MessageHandlers/MessageHandlerVersion1.js";
-
+import MessageHandlerVersion2 from "../Services/MessageHandlers/MessageHandlerVersion2.js";
 
 const router = Router();
 
@@ -17,6 +17,12 @@ router.get("/v0/messages", (req: Request, res: Response) => {
 // GET: /api/v1/messages?month=8&day=8
 router.get("/v1/messages", (req: Request, res: Response) => {
     const messageController = new MessageController(new MysqlMemberHandler(), new MessageHandlerVersion1());
+    messageController.getMessages(req, res);
+});
+
+// GET: /api/v2/messages?month=8&day=8
+router.get("/v2/messages", (req: Request, res: Response) => {
+    const messageController = new MessageController(new MysqlMemberHandler(), new MessageHandlerVersion2());
     messageController.getMessages(req, res);
 });
 
