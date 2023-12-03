b1cb7b3355338ee6848ba2ef872fde2857d954af
peifish1124 <sophiachen1124@gmail.com>
2023-12-03 17:47:28 +0800

    complete version-6 change output format

--- src/Controllers/XmlMessageController.ts
+++ src/Controllers/XmlMessageController.ts
@@ -4,31 +4,45 @@ import { Member } from "../Models/Member.js";
 import { Message } from "../Models/Message.js";
 import IMemberHandler from "../Services/MemberHandlers/IMemberHandler.js";
 import IMessageHandler from "../Services/MessageHandlers/IMessageHandler.js";
+import MessageController from "./MessageController.js";
 
-class MessageController {
-    protected readonly _memberHandler: IMemberHandler;
-    protected readonly _messageHandler: IMessageHandler;
+import { create } from "xmlbuilder";
 
+class XmlMessageController extends MessageController {
     constructor(memberHandler: IMemberHandler, messageHandler: IMessageHandler) {
-        this._memberHandler = memberHandler;
-        this._messageHandler = messageHandler;
+        super(memberHandler, messageHandler);
     }
 
     public async getMessages(req: Request, res: Response): Promise<void> {
         const month: number | undefined = req.query.month ? Number(req.query.month) : undefined;
         const day: number | undefined = req.query.day ? Number(req.query.day) : undefined;
-
+        
         const selectedMembers: Member[] = await this._memberHandler.getFilteredMembers(month, day);
         const messages: Message[] = selectedMembers.map((member) => this._messageHandler.generateMessage(member));
 
         if (messages.length > 0) {
+            const xmlString = this.convertMessagesToXml(messages);
+            res.set('Content-Type', 'text/xml');
             res.status(200);
-            res.json(messages);
+            res.send(xmlString);
         } else {
+            const xmlString = this.convertMessagesToXml(messages);
+            res.set('Content-Type', 'text/xml');
             res.status(404);
-            res.json(messages);
+            res.send(xmlString);
         }
+    }
+
+    private convertMessagesToXml(messages: Message[]): string {
+      const root = create('root');
+
+      messages.forEach((message) => {
+          root.ele('title', message.title);
+          root.ele('content', message.content);
+      });
+
+      return root.end({ indent: '    ', newline: '\n' });
     }
 }
 
-export default MessageController;
+export default XmlMessageController;
--- src/Routes/MessageRoute.ts
+++ src/Routes/MessageRoute.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from "express";
 
-import MessageController from "../Controllers/MessageController.js";
+import XmlMessageController from "../Controllers/XmlMessageController.js";
 import MockMemberHandler from "../Services/MemberHandlers/MockMemberHandler.js";
 import MongodbMemberHandler from "../Services/MemberHandlers/MongodbMemberHandler.js";
 import MessageHandlerVersion1 from "../Services/MessageHandlers/MessageHandlerVersion1.js";
@@ -12,31 +12,31 @@ const router = Router();
 
 // for testing without database
 router.get("/v0/messages", (req: Request, res: Response) => {
-    const messageController = new MessageController(new MockMemberHandler(), new MessageHandlerVersion1());
+    const messageController = new XmlMessageController(new MockMemberHandler(), new MessageHandlerVersion1());
     messageController.getMessages(req, res);
 });
 
 // GET: /api/v1/messages?month=8&day=8
 router.get("/v1/messages", (req: Request, res: Response) => {
-    const messageController = new MessageController(new MongodbMemberHandler(), new MessageHandlerVersion1());
+    const messageController = new XmlMessageController(new MongodbMemberHandler(), new MessageHandlerVersion1());
     messageController.getMessages(req, res);
 });
 
 // GET: /api/v2/messages?month=8&day=8
 router.get("/v2/messages", (req: Request, res: Response) => {
-    const messageController = new MessageController(new MongodbMemberHandler(), new MessageHandlerVersion2());
+    const messageController = new XmlMessageController(new MongodbMemberHandler(), new MessageHandlerVersion2());
     messageController.getMessages(req, res);
 });
 
 // GET: /api/v3/messages?month=12&day=22
 router.get("/v3/messages", (req: Request, res: Response) => {
-    const messageController = new MessageController(new MongodbMemberHandler(), new MessageHandlerVersion3());
+    const messageController = new XmlMessageController(new MongodbMemberHandler(), new MessageHandlerVersion3());
     messageController.getMessages(req, res);
 });
 
 // GET: /api/v4/messages?month=8&day=8
 router.get("/v4/messages", (req: Request, res: Response) => {
-    const messageController = new MessageController(new MongodbMemberHandler(), new MessageHandlerVersion4());
+    const messageController = new XmlMessageController(new MongodbMemberHandler(), new MessageHandlerVersion4());
     messageController.getMessages(req, res);
 });
 
