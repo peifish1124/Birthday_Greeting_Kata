597ae7740e5ae61d795ce1d2f6cf5e281b24873d
peifish1124 <sophiachen1124@gmail.com>
2023-12-03 17:17:50 +0800

    complete version-5 change database to mongodb

--- src/Routes/MessageRoute.ts
+++ src/Routes/MessageRoute.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from "express";
 
 import MessageController from "../Controllers/MessageController.js";
 import MockMemberHandler from "../Services/MemberHandlers/MockMemberHandler.js";
-import MysqlMemberHandler from "../Services/MemberHandlers/MysqlMemberHandler.js";
+import MongodbMemberHandler from "../Services/MemberHandlers/MongodbMemberHandler.js";
 import MessageHandlerVersion1 from "../Services/MessageHandlers/MessageHandlerVersion1.js";
 import MessageHandlerVersion2 from "../Services/MessageHandlers/MessageHandlerVersion2.js";
 import MessageHandlerVersion3 from "../Services/MessageHandlers/MessageHandlerVersion3.js";
@@ -18,25 +18,25 @@ router.get("/v0/messages", (req: Request, res: Response) => {
 
 // GET: /api/v1/messages?month=8&day=8
 router.get("/v1/messages", (req: Request, res: Response) => {
-    const messageController = new MessageController(new MysqlMemberHandler(), new MessageHandlerVersion1());
+    const messageController = new MessageController(new MongodbMemberHandler(), new MessageHandlerVersion1());
     messageController.getMessages(req, res);
 });
 
 // GET: /api/v2/messages?month=8&day=8
 router.get("/v2/messages", (req: Request, res: Response) => {
-    const messageController = new MessageController(new MysqlMemberHandler(), new MessageHandlerVersion2());
+    const messageController = new MessageController(new MongodbMemberHandler(), new MessageHandlerVersion2());
     messageController.getMessages(req, res);
 });
 
 // GET: /api/v3/messages?month=12&day=22
 router.get("/v3/messages", (req: Request, res: Response) => {
-    const messageController = new MessageController(new MysqlMemberHandler(), new MessageHandlerVersion3());
+    const messageController = new MessageController(new MongodbMemberHandler(), new MessageHandlerVersion3());
     messageController.getMessages(req, res);
 });
 
 // GET: /api/v4/messages?month=8&day=8
 router.get("/v4/messages", (req: Request, res: Response) => {
-    const messageController = new MessageController(new MysqlMemberHandler(), new MessageHandlerVersion4());
+    const messageController = new MessageController(new MongodbMemberHandler(), new MessageHandlerVersion4());
     messageController.getMessages(req, res);
 });
 
---
+++ src/Services/MemberHandlers/MongodbMemberHandler.ts
@@ -0,0 +1,55 @@
+import { MongoClient, Db } from 'mongodb';
+import { Member } from "../../Models/Member.js";
+
+import IMemberHandler from "./IMemberHandler.js";
+
+class MongodbMemberHandler implements IMemberHandler {
+    private client: MongoClient;
+    private db: Db;
+
+    constructor() {
+      const uri = process.env.MONGO_URL || '';
+      const dbName = process.env.MONGO_DB_NAME || '';
+
+      this.client = new MongoClient(uri);
+      this.db = this.client.db(dbName);
+    }
+
+    async getFilteredMembers(month: number | undefined, day: number | undefined): Promise<Member[]> {
+        const filteredMembers: Member[] = [];
+        try {
+            await this.client.connect();
+
+            const collection = this.db.collection('members');
+
+            const results = await collection.find({
+                $expr: {
+                    $and: [
+                        { $eq: [{ $month: '$date_of_birth' }, month || { $month: '$date_of_birth' }] },
+                        { $eq: [{ $dayOfMonth: '$date_of_birth' }, day || { $dayOfMonth: '$date_of_birth' }] },
+                    ],
+                },
+            }).toArray();
+
+            results.forEach((row: any) => {
+                const member: Member = {
+                    id: row._id,
+                    firstName: row.first_name,
+                    lastName: row.last_name,
+                    gender: row.gender,
+                    birthday: row.date_of_birth,
+                    email: row.email,
+                };
+                filteredMembers.push(member);
+            });
+
+            return filteredMembers;
+        } catch (error) {
+            throw new Error(`Error fetching members: ${error}`);
+        } finally {
+            await this.client.close();
+        }
+    }
+}
+
+export default MongodbMemberHandler;
