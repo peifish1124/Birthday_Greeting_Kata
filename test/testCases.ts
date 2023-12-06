5a623da7b04dcf090d3cc726bffcb1c89469f635
peifish1124 <sophiachen1124@gmail.com>
2023-12-07 03:40:41 +0800

    test: fix version-4

--- test/testCases.ts
+++ test/testCases.ts
@@ -151,6 +151,53 @@ export const generalTestCaseForVersion3: TestCase = {
   ]
 };
 
+export const generalTestCaseForVersion4: TestCase = {
+  testCases: [
+    {
+      request: { month: 4, day: 5 },
+      mockMemberData: [
+        { id: 3, firstName: 'Miki', lastName: 'Lai', gender: 'Female', birthday: new Date(1993, 4, 5), email: 'miki.lai@linecorp.com' }
+      ],
+      expectedOutput: [
+        {
+          title: 'Subject: Happy birthday!',
+          content: 'Happy birthday, dear Lai, Miki!',
+        },
+      ]
+    }, 
+    {
+      request: { month: 10, day: 10 },
+      mockMemberData: [
+        { id: 2, firstName: 'Cid', lastName: 'Change', gender: 'Male', birthday: new Date(1990, 10, 10), email: 'cid.change@linecorp.com' }
+      ],
+      expectedOutput: [
+        {
+          title: 'Subject: Happy birthday!',
+          content: 'Happy birthday, dear Change, Cid!',
+        },
+      ]
+    }, 
+    {
+      request: { month: 8, day: 8 },
+      mockMemberData: [
+        { id: 1, firstName: 'Robert', lastName: 'Yen', gender: 'Male', birthday: new Date(1985, 8, 8), email: 'robert.yen@linecorp.com' },
+        { id: 4, firstName: 'Sherry', lastName: 'Chen', gender: 'Female', birthday: new Date(1993, 8, 8), email: 'sherry.lai@linecorp.com' },
+      ],
+      expectedOutput: [
+        {
+          title: 'Subject: Happy birthday!',
+          content: 'Happy birthday, dear Yen, Robert!',
+        },
+        {
+          title: 'Subject: Happy birthday!',
+          content: 'Happy birthday, dear Chen, Sherry!',
+        },
+      ]
+    }
+
+  ]
+};
+
 export const notExistTestCase: TestCase = {
   testCases: [
     {
